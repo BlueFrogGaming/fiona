ced821374ef32655bdd0053c634a2edb5d83633e
Chad Remesch <chad@remesch.com>
2011-12-19 17:31:41 -0500

    Fix generator path

---
+++ lib/generators/fiona/templates/create_fiona_tables.rb
@@ -0,0 +1,21 @@
+class CreateFionaTables < ActiveRecord::Migration
+  def change
+    create_table :templates do |t|
+      t.string :type
+      t.string :key
+      t.string :name
+
+      t.timestamps
+    end
+
+    create_table :template_attributes do |t|
+      t.integer :template_id
+      t.string :key
+      t.string :value
+
+      t.timestamps
+    end
+
+    add_index :template_attributes, [:template_id, :key]
+  end
+end
