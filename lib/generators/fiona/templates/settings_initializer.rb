1684b7ab4c2d21015061e062d17e65c170e93afb
Chad Remesch <chad@remesch.com>
2011-12-20 15:22:30 -0500

    Temporarily disable settings initializer so migration will execute.

--- lib/generators/fiona/templates/settings_initializer.rb
+++ lib/generators/fiona/templates/settings_initializer.rb
@@ -1,10 +1,7 @@
-# Specify your default setting values here.
-defaults = {
-  app_name: 'My Awesome Rails App'
-}
-
-
-# Don't mess with this unless you know what you are doing.
-S = SettingsTemplate.find_or_initialize_by_key('settings')
-S.default_attributes = defaults
-S.save!
+# defaults = {
+#   app_name: 'My Awesome Rails App'
+# }
+#
+# S = SettingsTemplate.find_or_initialize_by_key('settings')
+# S.default_attributes = defaults
+# S.save!
